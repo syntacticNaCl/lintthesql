96e0beaab6c237c74defdd55ca5a8d2cc0374aa0
David Posey <david@shootproof.com>
2018-06-27 16:22:54 -0400

    Add nyan progress bar

--- lintthesql/__init__.py
+++ lintthesql/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import sys, os, argparse
+import sys, os, argparse, time
 
 from config import Config
 from parser import Parser
 from pathlib import Path
+from nyanbar import NyanBar
 
 def lintthesql(args):
     cwd = os.getcwd()
@@ -21,10 +22,17 @@ def lintthesql(args):
     parser = Parser(config)
     input_file = cwd + '/' + args.file
     input_file_path = Path(input_file)
+    nyan_progress = None
 
     if input_file_path.is_file():
         parse(parser, input_file)
     elif input_file_path.is_dir():
+        input_files = []
+
+        if args.nyan:
+            nyan_mp3_file = os.path.dirname(__file__) + '/nyan.mp3'
+            nyan_progress = NyanBar(audiofile=nyan_mp3_file)
+
         for root, dirs, files in os.walk(input_file_path):
             path = root.split(os.sep)
 
@@ -32,17 +40,32 @@ def lintthesql(args):
                 input_file = root + '/' + file
 
                 if os.path.splitext(input_file)[1] == '.' + args.type.replace('.', ''):
-                    parse(parser, input_file)
+                    input_files.append(input_file)
+
+        total_input_files = len(input_files)
+
+        for input_file in input_files:
+            parse(parser, input_file)
+
+            input_file_index = input_files.index(input_file)
+            progress = (input_file_index + 1) / total_input_files * 100
+
+            if nyan_progress:
+                nyan_progress.update(progress)
     else:
         # doesn't exist
         print('Input file does not exist you dummy!')
         sys.exit()
 
-def parse_args():
-    arg_parser = argparse.ArgumentParser(description='Lint some SQL.')
+    if nyan_progress:
+        nyan_progress.finish()
 
-    arg_parser.add_argument('--file', default='./', help='lints the given .sql file')
+def parse_args():
+    arg_parser = argparse.ArgumentParser(description='Lint, the S(e)Q(ue)L - a configurable SQL linter.')
+
+    arg_parser.add_argument('--file', default='./', help='lints the given file (default: lints all files)')
     arg_parser.add_argument('--type', default='sql', help='sets the type of files to lint (default: .sql)')
+    arg_parser.add_argument('--nyan', default=False, action='store_true', help='show a Nyan Cat progress bar')
 
     return arg_parser.parse_args()
 
