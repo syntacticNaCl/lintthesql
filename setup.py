072664985f2cc3ce34a14156cc2bb91c0da33d68
David Posey <david@shootproof.com>
2018-07-02 09:44:43 -0400

    Update reporter import and include mp3 file

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
     ],
+    package_data= {'': ['nyan.mp3']},
     keywords = 'cli, lint, formatter, sql',
     packages = find_packages(exclude=['docs', 'tests*', 'examples']),
     install_requires = ['sqlparse','PyYaml'],
